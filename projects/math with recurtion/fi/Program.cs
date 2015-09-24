a43ccea1a9e8cd629dd3f8de25ecef899fe26676
martin_varbanov96 <martin_varbanov96@abv.bg>
2015-09-24 21:44:04 +0300

    fibunachi
    Added fibunachi exercises

---
+++ projects/math with recurtion/fi/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("max value= ");
+        int max = int.Parse(Console.ReadLine());
+
+        Console.Write("start value= ");
+        int start = int.Parse(Console.ReadLine());
+
+        fibunachiRow(start, max, 0, 0);
+    }
+
+    private static void fibunachiRow(int start, int max, int second, int third)
+    {
+        if(third==0)
+        {
+            fibunachiRow(start, max, start, (start+start));
+            return;
+        }
+
+        if ((start <= max) && (second <= max) && (third<=max))
+        {
+            Console.WriteLine(" {0} ",start);
+            fibunachiRow((start + second), max, start, second);
+        }
+    }
+}
---
+++ projects/math with recurtion/fiFastWihoutRecursion/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{ 
+    static void Main(string[] args)
+    {
+        Console.Write("max value= ");
+        int max = int.Parse(Console.ReadLine());
+
+        Console.Write("start value= ");
+        int start = int.Parse(Console.ReadLine());
+
+        fiRow(start, max);
+    }
+
+    private static void fiRow(int start, int max)
+    {
+        int b = 0;
+
+        while(start<=max)
+        {
+            Console.Write(" {0} ", start);
+            start += b;
+            b = start - b;
+        }
+    }
+}
