dc67ea6298c05ae6c3cead5a0c7e6ec7855064e4
martin_varbanov96 <martin_varbanov96@abv.bg>
2015-09-25 09:44:37 +0300

    fi
    proving fi=1,6

---
+++ projects/math with recurtion/provingFi/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter max value= ");
+        decimal max = decimal.Parse(Console.ReadLine());
+
+        Console.Write("Enter start value= ");
+        decimal start = decimal.Parse(Console.ReadLine());
+
+        getAvgFi(start, max, 0, 0);
+    }
+
+    private static void getAvgFi(decimal start, decimal max, decimal b, decimal c)
+    {
+        if (c == 0)
+        {
+            getAvgFi(start + start, max, start, start);
+            return;
+        }
+        if(start <= max)
+        {
+            Console.WriteLine("A= {0}, B= {1}, avg(fi)=A/B= {2}",start, b, (start/b));
+            getAvgFi((start + b), max, (b + c), (start-c));
+        }
+    }
+}
